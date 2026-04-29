2e3a95138b97a5285379f2c4a44e60c70f56919a
Hugo Gruson <git@hugogruson.fr>
2026-04-29 16:59:43 +0200

    Add tests

---
+++ tests/testthat/test-read_npy.R
@@ -0,0 +1,85 @@
+test_that("different types work", {
+  system.file("extdata", "test_int32.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(array(c(1L, 2L, 3L), dim = 3L))
+
+  system.file("extdata", "test_int8.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(array(c(-128L, 0L, 127L), dim = 3L))
+
+  system.file("extdata", "test_int16.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(array(c(-32768L, 0L, 32767L), dim = 3L))
+
+  # FIXME: support int64
+  # system.file("extdata", "test_int64.npy", package = "grumpy") |>
+  #   read_npy() |>
+  #   expect_no_condition() |>
+  #   expect_identical(array(c(-2^32, 0, 2^32), dim = 3L))
+
+  system.file("extdata", "test_uint8.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(array(c(0L, 128L, 255L), dim = 3L))
+
+  system.file("extdata", "test_uint16.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(array(c(0L, 1000L, 65535L), dim = 3L))
+
+  system.file("extdata", "test_float32.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_equal(array(c(1.5, -2.5, 3.14), dim = 3L), tolerance = 1e-6)
+
+  system.file("extdata", "test_float64.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(array(c(1.5, -2.5, 3.14), dim = 3L))
+
+  system.file("extdata", "test_bool.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(array(c(TRUE, FALSE, TRUE), dim = 3L))
+})
+
+test_that("big endian files work", {
+  system.file("extdata", "test_bigendian.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(array(c(1.0, 2.0, 3.0), dim = 3L))
+})
+
+test_that("higher-dimension arrays work", {
+  system.file("extdata", "test_2d.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(array(0:5, dim = c(2L, 3L)))
+
+  system.file("extdata", "test_3d.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(array(0:23, dim = c(2L, 3L, 4L)))
+})
+
+test_that("fortran order arrays work", {
+  system.file("extdata", "test_fortran.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(matrix(0:5, nrow = 2L, ncol = 3L, byrow = TRUE))
+})
+
+test_that("scalar or empty arrays work", {
+  system.file("extdata", "test_scalar.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_identical(array(42))
+
+  system.file("extdata", "test_empty.npy", package = "grumpy") |>
+    read_npy() |>
+    expect_no_condition() |>
+    expect_length(0)
+})
