4c13302394acea768a29163bc2bc17648e242555
Hugo Gruson <git@hugogruson.fr>
2026-04-29 15:49:27 +0200

    First commit

---
+++ R/read_npy.R
@@ -0,0 +1,79 @@
+#' Read a .npy file
+#'
+#' @param path Path to the .npy file
+#' 
+#' @return An array containing the data from the .npy file
+#' 
+#' @export
+#' 
+#' @examples
+#' read_npy(
+#'   system.file("extdata", "test.npy", package = "grumpy")
+#' )
+
+read_npy <- function(path) {
+  if (!file.exists(path)) {
+    stop("File does not exist: ", path)
+  }
+  con <- file(path, "rb")
+  on.exit(close(con))
+  
+  # Read the header
+  magic_string <- readBin(con, "raw", n = 6)
+  if (!identical(magic_string, charToRaw("\x93NUMPY"))) {
+    stop("Not a valid .npy file: ", path) 
+  }
+
+  version <- readBin(con, "integer", n = 2, size = 1, endian = "little")
+
+  header_len <- if (version[1] == 1) {
+    readBin(con, "integer", n = 1, size = 2, endian = "little")
+  } else if (version[1] %in% c(2, 3)) {
+    readBin(con, "integer", n = 1, size = 4, endian = "little")
+  } else {
+    stop("Unsupported .npy version: ", version[1])
+  }
+
+  header <- rawToChar(readBin(con, "raw", n = header_len))
+
+  descr <- regmatches(header, regexec("['\"]descr['\"]\\s*:\\s*['\"]([<|>]?)([?bBiufcmMOSaUV])(\\d+)['\"]", header, perl = TRUE))[[1]]
+  endianness <- if (descr[2] %in% c("", "|")) {
+    .Platform$endian
+  } else {
+    switch(
+      descr[2],
+      "<" = "little",
+      ">" = "big",
+      stop("Invalid endianness in .npy file: ", descr[1])
+    )
+  }
+  type <- switch(
+    descr[3],
+    "f" = "numeric",
+    "i" = "integer",
+    "u" = "integer",
+    "b" = "logical",
+    stop("Unsupported data type in .npy file: ", descr[1])
+  )
+  size <- as.integer(descr[4])
+
+  fortran_order <- as.logical(regmatches(header, regexec("['\"]fortran_order['\"]\\s*:\\s*(True|False)", header))[[1]][2])
+  shape <- regmatches(header, regexec("['\"]shape['\"]\\s*:\\s*\\(([^\\)]*)\\)", header))[[1]][2]
+  shape <- as.integer(strsplit(shape, ",\\s*")[[1]])
+
+  if (!fortran_order) {
+    shape <- rev(shape)
+  }
+
+  # Read the data
+  num_elements <- prod(shape)
+  data <- readBin(con, what = type, n = num_elements, size = size, endian = endianness)
+
+  dim(data) <- shape
+
+  if (!fortran_order) {
+    data <- aperm(data, rev(seq_along(shape)))
+  }
+
+  return(data)
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+
+
+<!-- README.md is generated from README.qmd. Please edit that file -->
+
+# grumpy
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+The goal of grumpy is to …
+
+## Installation
+
+You can install the development version of grumpy like so:
+
+``` r
+# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+```
+
+## Example
