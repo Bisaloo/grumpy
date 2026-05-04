40c7f9a4319b2de99a8c6d7de29fd938e4f989ef
Hugo Gruson <git@hugogruson.fr>
2026-05-04 08:13:42 +0200

    Use larger test datasets

--- inst/scripts/generate_test_data.py
+++ inst/scripts/generate_test_data.py
@@ -1,36 +1,36 @@
 import numpy as np
 
 # Integer types
-np.save("inst/extdata/test_int32.npy", np.array([1, 2, 3], dtype="int32"))
-np.save("inst/extdata/test_int8.npy", np.array([-128, 0, 127], dtype="int8"))
-np.save("inst/extdata/test_int16.npy", np.array([-32768, 0, 32767], dtype="int16"))
-np.save("inst/extdata/test_int64.npy", np.array([-2, 73, 38], dtype="int64"))
-np.save("inst/extdata/test_int64_overflowing.npy", np.array([-2**32, 0, 2**32], dtype="int64"))
+np.save("inst/extdata/test_int32.npy", np.arange(1, 13, dtype="int32"))
+np.save("inst/extdata/test_int8.npy", np.array([-128, -100, -64, -32, -16, -8, -4, -2, -1, 0, 64, 127], dtype="int8"))
+np.save("inst/extdata/test_int16.npy", np.array([-32768, -16384, -8192, -4096, -2048, -1024, -512, -256, -128, 0, 16383, 32767], dtype="int16"))
+np.save("inst/extdata/test_int64.npy", np.arange(-5, 7, dtype="int64"))
+np.save("inst/extdata/test_int64_overflowing.npy", np.array([-2**32, -2**31, -2**16, -2**8, 0, 1, 2**8, 2**16, 2**31, 2**32, 2**48, 2**63 - 1], dtype="int64"))
 
 # Unsigned integer (stored as integer in R)
-np.save("inst/extdata/test_uint8.npy", np.array([0, 128, 255], dtype="uint8"))
-np.save("inst/extdata/test_uint16.npy", np.array([0, 1000, 65535], dtype="uint16"))
-np.save("inst/extdata/test_uint32.npy", np.array([0, 100000, 2**32 - 1], dtype="uint32"))
-np.save("inst/extdata/test_uint64.npy", np.array([0, 100000, 2**64 - 1], dtype="uint64"))
+np.save("inst/extdata/test_uint8.npy", np.array([0, 10, 20, 50, 100, 128, 150, 175, 200, 210, 240, 255], dtype="uint8"))
+np.save("inst/extdata/test_uint16.npy", np.array([0, 100, 1000, 5000, 10000, 20000, 32767, 40000, 50000, 55000, 60000, 65535], dtype="uint16"))
+np.save("inst/extdata/test_uint32.npy", np.array([0, 1, 10, 100, 1000, 10000, 100000, 1000000, 2**16, 2**24, 2**31, 2**32 - 1], dtype="uint32"))
+np.save("inst/extdata/test_uint64.npy", np.array([0, 1, 10, 100, 1000, 10000, 100000, 2**16, 2**32, 2**48, 2**63, 2**64 - 1], dtype="uint64"))
 
 # Float types
-np.save("inst/extdata/test_float32.npy", np.array([1.5, -2.5, 3.14], dtype="float32"))
-np.save("inst/extdata/test_float64.npy", np.array([1.5, -2.5, 3.14], dtype="float64"))
+np.save("inst/extdata/test_float32.npy", np.array([1.5, -2.5, 3.14, 0.0, -1.0, 100.0, -100.0, 0.001, 1e6, -1e6, 1.23456, -9.87654], dtype="float32"))
+np.save("inst/extdata/test_float64.npy", np.array([1.5, -2.5, 3.14, 0.0, -1.0, 100.0, -100.0, 0.001, 1e6, -1e6, 1.23456789, -9.87654321], dtype="float64"))
 
 # Boolean
-np.save("inst/extdata/test_bool.npy", np.array([True, False, True], dtype="bool"))
+np.save("inst/extdata/test_bool.npy", np.array([True, False, True, True, False, False, True, False, True, True, False, True], dtype="bool"))
 
 # Big-endian
-np.save("inst/extdata/test_bigendian.npy", np.array([1.0, 2.0, 3.0], dtype=">f4"))
+np.save("inst/extdata/test_bigendian.npy", np.arange(1.0, 13.0, dtype=">f4"))
 
 # 2-D array (C order)
-np.save("inst/extdata/test_2d.npy", np.arange(6, dtype="int32").reshape(2, 3))
+np.save("inst/extdata/test_2d.npy", np.arange(12, dtype="int32").reshape(3, 4))
 
 # 3-D array
 np.save("inst/extdata/test_3d.npy", np.arange(24, dtype="int32").reshape(2, 3, 4))
 
 # Fortran order (column-major)
-np.save("inst/extdata/test_fortran.npy", np.asfortranarray(np.arange(6, dtype="int32").reshape(2, 3)))
+np.save("inst/extdata/test_fortran.npy", np.asfortranarray(np.arange(12, dtype="int32").reshape(3, 4)))
 
 # Single element
 np.save("inst/extdata/test_scalar.npy", np.array([42.0], dtype="float32"))
@@ -39,8 +39,8 @@ np.save("inst/extdata/test_scalar.npy", np.array([42.0], dtype="float32"))
 np.save("inst/extdata/test_empty.npy", np.array([], dtype="float32"))
 
 # String types (unicode and byte strings)
-np.save("inst/extdata/test_str_unicode.npy", np.array(["foo", "bar", "baz"], dtype="U3"))
-np.save("inst/extdata/test_str_bytes.npy", np.array([b"foo", b"bar", b"baz"], dtype="S3"))
+np.save("inst/extdata/test_str_unicode.npy", np.array(["foo", "bar", "baz", "qux", "hello", "world", "alpha", "beta", "gamma", "delta", "epsilon", "zeta"], dtype="U7"))
+np.save("inst/extdata/test_str_bytes.npy", np.array([b"foo", b"bar", b"baz", b"qux", b"hello", b"world", b"alpha", b"beta", b"gamma", b"delta", b"eps", b"zeta"], dtype="S5"))
 
 # NPZ archive (multiple arrays)
 np.savez(
--- tests/testthat/test-read_npy.R
+++ tests/testthat/test-read_npy.R
@@ -2,61 +2,150 @@ test_that("different types work", {
   system.file("extdata", "test_int32.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(array(c(1L, 2L, 3L), dim = 3L))
+    expect_identical(array(1L:12L, dim = 12L))
 
   system.file("extdata", "test_int8.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(array(c(-128L, 0L, 127L), dim = 3L))
+    expect_identical(array(
+      c(-128L, -100L, -64L, -32L, -16L, -8L, -4L, -2L, -1L, 0L, 64L, 127L),
+      dim = 12L
+    ))
 
   system.file("extdata", "test_int16.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(array(c(-32768L, 0L, 32767L), dim = 3L))
+    expect_identical(array(
+      c(
+        -32768L,
+        -16384L,
+        -8192L,
+        -4096L,
+        -2048L,
+        -1024L,
+        -512L,
+        -256L,
+        -128L,
+        0L,
+        16383L,
+        32767L
+      ),
+      dim = 12L
+    ))
 
   system.file("extdata", "test_int64.npy", package = "grumpy") |>
     read_npy() |>
-    expect_identical(array(c(-2L, 73L, 38L), dim = 3L)) |>
+    expect_identical(array(-5L:6L, dim = 12L)) |>
     expect_warning("overflow")
 
   system.file("extdata", "test_uint8.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(array(c(0L, 128L, 255L), dim = 3L))
+    expect_identical(array(
+      c(0L, 10L, 20L, 50L, 100L, 128L, 150L, 175L, 200L, 210L, 240L, 255L),
+      dim = 12L
+    ))
 
   system.file("extdata", "test_uint16.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(array(c(0L, 1000L, 65535L), dim = 3L))
+    expect_identical(array(
+      c(
+        0L,
+        100L,
+        1000L,
+        5000L,
+        10000L,
+        20000L,
+        32767L,
+        40000L,
+        50000L,
+        55000L,
+        60000L,
+        65535L
+      ),
+      dim = 12L
+    ))
 
   system.file("extdata", "test_float32.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_equal(array(c(1.5, -2.5, 3.14), dim = 3L), tolerance = 1e-6)
+    expect_equal(
+      array(
+        c(
+          1.5,
+          -2.5,
+          3.14,
+          0.0,
+          -1.0,
+          100.0,
+          -100.0,
+          0.001,
+          1e6,
+          -1e6,
+          1.23456,
+          -9.87654
+        ),
+        dim = 12L
+      ),
+      tolerance = 1e-6
+    )
 
   system.file("extdata", "test_float64.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(array(c(1.5, -2.5, 3.14), dim = 3L))
+    expect_identical(array(
+      c(
+        1.5,
+        -2.5,
+        3.14,
+        0.0,
+        -1.0,
+        100.0,
+        -100.0,
+        0.001,
+        1e6,
+        -1e6,
+        1.23456789,
+        -9.87654321
+      ),
+      dim = 12L
+    ))
 
   system.file("extdata", "test_bool.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(array(c(TRUE, FALSE, TRUE), dim = 3L))
+    expect_identical(array(
+      c(
+        TRUE,
+        FALSE,
+        TRUE,
+        TRUE,
+        FALSE,
+        FALSE,
+        TRUE,
+        FALSE,
+        TRUE,
+        TRUE,
+        FALSE,
+        TRUE
+      ),
+      dim = 12L
+    ))
 })
 
 test_that("big endian files work", {
   system.file("extdata", "test_bigendian.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(array(c(1.0, 2.0, 3.0), dim = 3L))
+    expect_identical(array(as.double(1:12), dim = 12L))
 })
 
 test_that("higher-dimension arrays work", {
   system.file("extdata", "test_2d.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(array(0:5, dim = c(2L, 3L)))
+    expect_identical(array(0:11, dim = c(3L, 4L)))
 
   system.file("extdata", "test_3d.npy", package = "grumpy") |>
     read_npy() |>
@@ -68,19 +157,51 @@ test_that("fortran order arrays work", {
   system.file("extdata", "test_fortran.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(matrix(0:5, nrow = 2L, ncol = 3L, byrow = TRUE))
+    expect_identical(matrix(0:11, nrow = 3L, ncol = 4L, byrow = TRUE))
 })
 
 test_that("string dtypes raise an informative error", {
   system.file("extdata", "test_str_unicode.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(array(c("foo", "bar", "baz"), dim = 3L))
+    expect_identical(array(
+      c(
+        "foo",
+        "bar",
+        "baz",
+        "qux",
+        "hello",
+        "world",
+        "alpha",
+        "beta",
+        "gamma",
+        "delta",
+        "epsilon",
+        "zeta"
+      ),
+      dim = 12L
+    ))
 
   system.file("extdata", "test_str_bytes.npy", package = "grumpy") |>
     read_npy() |>
     expect_no_condition() |>
-    expect_identical(array(c("foo", "bar", "baz"), dim = 3L))
+    expect_identical(array(
+      c(
+        "foo",
+        "bar",
+        "baz",
+        "qux",
+        "hello",
+        "world",
+        "alpha",
+        "beta",
+        "gamma",
+        "delta",
+        "eps",
+        "zeta"
+      ),
+      dim = 12L
+    ))
 })
 
 test_that("scalar or empty arrays work", {
