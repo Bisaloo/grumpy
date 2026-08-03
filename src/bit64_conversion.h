97fe0a77ba7fa9ad65eab67d333e794fb2c2235f
Hugo Gruson <git@hugogruson.fr>
2026-08-03 14:08:17 +0200

    Split loop to enable auto-vectorization

--- src/bit64_conversion.h
+++ src/bit64_conversion.h
@@ -5,15 +5,16 @@ static inline void uint32_to_int32(const void* restrict in_buf, size_t n, void* 
   const uint32_t* restrict in = (const uint32_t *)in_buf;
   int32_t* restrict out = (int32_t *)out_buf;
   R_xlen_t i;
-  R_xlen_t n_overflow = 0;
 
+  // This needs to be done in two passes to allow for auto-vectorization of the conversion loop by
+  // the compiler.
   for (i = 0; i < n; i++) {
-    if (in[i] > INT_MAX) {
-      out[i] = INT_MIN; // NA
-      n_overflow++;
-    } else {
-      out[i] = in[i];
-    }
+    out[i] = (in[i] > INT_MAX) ? INT_MIN : (int32_t)in[i];
+  }
+
+  R_xlen_t n_overflow = 0;
+  for (i = 0; i < n; i++) {
+    n_overflow += (in[i] > INT_MAX);
   }
 
   if (n_overflow > 0) {
@@ -27,23 +28,21 @@ static inline void uint32_to_int32(const void* restrict in_buf, size_t n, void* 
 
 static inline void int64_to_int32(const void* restrict in_buf, size_t n, void* restrict out_buf) {
 
+  const int64_t* restrict in = (const int64_t *)in_buf;
   int32_t* restrict out = (int32_t *)out_buf;
   R_xlen_t i;
+
+  // This needs to be done in two passes to allow for auto-vectorization of the conversion loop by
+  // the compiler.
+  for (i = 0; i < n; i++) {
+    out[i] = (in[i] > INT_MAX || in[i] < INT_MIN) ? INT_MIN : (int32_t)in[i];
+  }
+  
   R_xlen_t n_overflow = 0;
   R_xlen_t n_underflow = 0;
-
-  const int64_t* restrict in = (const int64_t *)in_buf;
-  for (i=0; i<n; i++) {
-    if (in[i] > INT_MAX) {
-      out[i] = INT_MIN; // NA
-      n_overflow++;
-    }
-    else if (in[i] < INT_MIN) {
-      out[i] = INT_MIN; // NA
-      n_underflow++;
-    } else {
-      out[i] = in[i];
-    }
+  for (i = 0; i < n; i++) {
+    n_overflow += (in[i] > INT_MAX);
+    n_underflow += (in[i] < INT_MIN);
   }
 
   if (n_overflow > 0) {
@@ -66,15 +65,16 @@ static inline void uint64_to_int32(const void* restrict in_buf, size_t n, void* 
   const uint64_t* restrict in = (const uint64_t *)in_buf;
   int32_t* restrict out = (int32_t *)out_buf;
   R_xlen_t i;
-  R_xlen_t n_overflow = 0;
 
-  for (i=0; i<n; i++) {
-    if (in[i] > INT_MAX) {
-      out[i] = INT_MIN; // NA
-      n_overflow++;
-    } else {
-      out[i] = in[i];
-    }
+  // This needs to be done in two passes to allow for auto-vectorization of the conversion loop by
+  // the compiler.
+  for (i = 0; i < n; i++) {
+    out[i] = (in[i] > INT_MAX) ? INT_MIN : (int32_t)in[i];
+  }
+
+  R_xlen_t n_overflow = 0;
+  for (i = 0; i < n; i++) {
+    n_overflow += (in[i] > INT_MAX);
   }
 
   if (n_overflow > 0) {
@@ -83,5 +83,5 @@ static inline void uint64_to_int32(const void* restrict in_buf, size_t n, void* 
       n_overflow
     );
   }
-  
+
 }
