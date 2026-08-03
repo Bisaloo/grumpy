d5af5fa2f344a25406dcd835d15b8505ead411da
Hugo Gruson <git@hugogruson.fr>
2026-08-03 11:40:36 +0200

    Use restrict and avoid recasting pointer

--- src/bit64_conversion.c
+++ src/bit64_conversion.c
@@ -1,17 +1,19 @@
 #include "bit64_conversion.h"
 
 
-void uint32_to_int32(const void* in_buf, size_t n, void* out_buf) {
-  
+void uint32_to_int32(const void* restrict in_buf, size_t n, void* restrict out_buf) {
+
+  const uint32_t* restrict in = (const uint32_t *)in_buf;
+  int32_t* restrict out = (int32_t *)out_buf;
   R_xlen_t i;
   R_xlen_t n_overflow = 0;
 
   for (i = 0; i < n; i++) {
-    if (((uint32_t *)in_buf)[i] > INT_MAX) {
-      ((int32_t *)out_buf)[i] = INT_MIN;
+    if (in[i] > INT_MAX) {
+      out[i] = INT_MIN;
       n_overflow++;
     } else {
-      ((int32_t *)out_buf)[i] = ((uint32_t *)in_buf)[i];
+      out[i] = in[i];
     }
   }
 
@@ -24,32 +26,35 @@ void uint32_to_int32(const void* in_buf, size_t n, void* out_buf) {
   
 }
 
-void int64_to_int32(const void* in_buf, size_t n, void* out_buf, bool is_signed) {
-  
+void int64_to_int32(const void* restrict in_buf, size_t n, void* restrict out_buf, bool is_signed) {
+
+  int32_t* restrict out = (int32_t *)out_buf;
   R_xlen_t i;
   R_xlen_t n_overflow = 0;
   R_xlen_t n_underflow = 0;
-  
+
   if (is_signed) {
+    const int64_t* restrict in = (const int64_t *)in_buf;
     for (i=0; i<n; i++) {
-      if (((int64_t *)in_buf)[i] > INT_MAX) {
-        ((int32_t *)out_buf)[i] = INT_MIN;
+      if (in[i] > INT_MAX) {
+        out[i] = INT_MIN;
         n_overflow++;
       }
-      else if (((int64_t *)in_buf)[i] < INT_MIN) {
-        ((int32_t *)out_buf)[i] = INT_MIN;
+      else if (in[i] < INT_MIN) {
+        out[i] = INT_MIN;
         n_underflow++;
       } else {
-        ((int32_t *)out_buf)[i] = ((int64_t *)in_buf)[i];
+        out[i] = in[i];
       }
     }
   } else {
+    const uint64_t* restrict in = (const uint64_t *)in_buf;
     for (i=0; i<n; i++) {
-      if (((uint64_t *)in_buf)[i] > INT_MAX) {
-        ((int32_t *)out_buf)[i] = INT_MIN;
+      if (in[i] > INT_MAX) {
+        out[i] = INT_MIN;
         n_overflow++;
       } else {
-        ((int *)out_buf)[i] = ((uint64_t *)in_buf)[i];
+        out[i] = in[i];
       }
     }
   }
--- src/bit64_conversion.h
+++ src/bit64_conversion.h
@@ -1,4 +1,4 @@
 #include "grumpy.h"
 
-void uint32_to_int32(const void* in_buf, size_t n, void* out_buf);
-void  int64_to_int32(const void* in_buf, size_t n, void* out_buf, bool is_signed);
+void uint32_to_int32(const void* restrict in_buf, size_t n, void* restrict out_buf);
+void  int64_to_int32(const void* restrict in_buf, size_t n, void* restrict out_buf, bool is_signed);
