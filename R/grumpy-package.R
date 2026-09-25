e57744705c5e3b2f4c253bf3203eab44fb19f9da
Hugo Gruson <git@hugogruson.fr>
2026-09-25 19:11:26 +0200

    Add backports

--- R/grumpy-package.R
+++ R/grumpy-package.R
@@ -8,3 +8,13 @@
 ## mockable bindings: start
 ## mockable bindings: end
 NULL
+
+# Backport from R 4.4.0
+"%||%" <- function(x, y) {
+  if (is.null(x)) y else x # nolint: coalesce_linter.
+}
+
+# Backport from R 4.6.0
+`%notin%` <- function(x, table) {
+  match(x, table, nomatch = 0L) == 0L
+}
