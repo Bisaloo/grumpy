44f458cbb10b26c70bf750a64ee13d743e43cdbc
Hugo Gruson <git@hugogruson.fr>
2026-09-25 14:09:38 +0200

    Implement read_npz(arrays = ) arg for partial npz read

--- R/read_npz.R
+++ R/read_npz.R
@@ -1,6 +1,9 @@
 #' Read a `.npz` file
 #'
 #' @param file Path to the `.npz` file
+#' @param arrays Optional character vector of array names or numeric vector of
+#'   indices to read from the `.npz` file. If `NULL` (the default), all arrays
+#'   are read.
 #'
 #' @return A named list of arrays containing the data from the `.npz` file
 #'
@@ -13,18 +16,38 @@
 #' read_npz(
 #'   system.file("extdata", "test.npz", package = "grumpy")
 #' )
+#'
+#' read_npz(
+#'   system.file("extdata", "test.npz", package = "grumpy"),
+#'   arrays = "x"
+#' )
+#'
 
-read_npz <- function(file) {
+read_npz <- function(file, arrays = NULL) {
   if (!file.exists(file)) {
     stop("File does not exist: ", file, call. = FALSE)
   }
   files <- unzip(file, list = TRUE)
+  all_arrays <- sub("\\.npy$", "", files$Name)
+
+  to_extract <- arrays %||% all_arrays
+  if (is.numeric(to_extract)) {
+    to_extract <- all_arrays[to_extract]
+  }
+
+  if (!all(to_extract %in% all_arrays)) {
+    stop(
+      "The following arrays are not present in the .npz file: ",
+      paste(setdiff(to_extract, all_arrays), collapse = ", "),
+      call. = FALSE
+    )
+  }
 
   # Read each .npy file in the .npz archive
-  lapply(files$Name, function(name) {
+  lapply(paste0(to_extract, ".npy"), function(name) {
     con <- unz(file, name, "rb")
     on.exit(close(con))
     read_npy(con)
   }) |>
-    setNames(gsub("\\.npy$", "", files$Name))
+    setNames(to_extract)
 }
--- tests/testthat/test-read_npz.R
+++ tests/testthat/test-read_npz.R
@@ -10,3 +10,20 @@ test_that("read_npz() works", {
   expect_identical(res[[1L]], array(c(1L, 2L, 3L), dim = 3L))
   expect_identical(res[[2L]], array(c(4.0, 5.0, 6.0), dim = 3L))
 })
+
+test_that("read_npz() works with arrays argument", {
+  full_read <- system.file("extdata", "test.npz", package = "grumpy") |>
+    read_npz()
+
+  partial_character <- system.file("extdata", "test.npz", package = "grumpy") |>
+    read_npz(arrays = "y") |>
+    expect_no_condition()
+
+  expect_identical(partial_character[[1L]], full_read[["y"]])
+
+  partial_numeric <- system.file("extdata", "test.npz", package = "grumpy") |>
+    read_npz(arrays = 2L) |>
+    expect_no_condition()
+
+  expect_identical(partial_numeric[[1L]], full_read[["y"]])
+})
